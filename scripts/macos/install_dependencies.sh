a61ad750565bbe89c074c75c666bb54f5bdeb07f
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2025-10-01 16:11:32 +0200

    Fix install_dependencies.sh script for macos locally

--- scripts/macos/install_dependencies.sh
+++ scripts/macos/install_dependencies.sh
@@ -11,7 +11,8 @@ install_prefix=/opt
 googletest_version=1.10.0
 
 echo "Downloading sources"
-curl -O https://github.com/google/googletest/archive/refs/tags/release-${googletest_version}.zip
+# curl -O https://github.com/google/googletest/archive/refs/tags/release-${googletest_version}.zip
+curl -o release-${googletest_version}.zip https://codeload.github.com/google/googletest/zip/refs/tags/release-${googletest_version} 
 
 # Alternatively you can install googletest simply via: "brew install googletest" - but if you do - you do it on your own risk (version incompatibility is possible)
 echo "Installing googletest"
@@ -20,7 +21,7 @@ rm release-${googletest_version}.zip
 pushd googletest-release-${googletest_version}
 mkdir releasebuild
 cd releasebuild
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=${install_prefix} ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=${install_prefix} -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..
 make -j${njobs} install
 popd
 rm -rf googletest-release-${googletest_version}
